d13d9129777b3f324454f084cbd0f5d4fe4b37eb
tlewandowski18 <tlewandowski18@gmail.com>
2020-07-16 19:45:35 -0400

    complete first-pass moving-zeroes algorithm

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,22 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+#Objective - move all zeros to right end of list and all non-zeroes to left end
 def moving_zeroes(arr):
-    # Your code here
+    #Your code here
+    #create a new array
+    new_arr = []
+    #iterate through list:
+    for i in range(len(arr)):
+        #if item does equals zero add zero to end of list
+        if arr[i] == 0: 
+            new_arr.append(arr[i])
+        #if item does not equal zero add item to beginning of list
+        else:
+            new_arr.insert(0, arr[i])
+            
+    return new_arr
 
-    pass
 
 
 if __name__ == '__main__':
