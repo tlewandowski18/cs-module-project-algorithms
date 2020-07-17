8f956a08bbec7a217c1a49c183765103927532fc
tlewandowski18 <tlewandowski18@gmail.com>
2020-07-16 20:21:41 -0400

    complete first pass product_of_all_other_numbers and sliding_window_max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,8 +4,19 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    #create new_array
+    new_array = []
+    #iterate through given arr:
+    for i in range(len(nums)):
+        #create sub_array that contains values from given array starting at current index and running up to but not including current index + k
+        sub_array = nums[i:i+k]
+        #make sure sub_array has length of k
+        if len(sub_array) == k:
+            #if so append max of sub_array to new_array
+            new_array.append(max(sub_array))
+    return new_array
 
-    pass
+   
 
 
 if __name__ == '__main__':
